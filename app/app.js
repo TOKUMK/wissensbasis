fd27a48c5403122b4f3f09c0080c277ca3bc71bb
TOKUMK <jcaseyire@gmail.com>
2017-06-30 00:16:35 +0700

    ui changes

--- app/app.js
+++ app/app.js
@@ -3,9 +3,14 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
+
 // database
 mongoose.connect('mongodb://localhost/wissensbasis');
 let db = mongoose.connection;
+
+
+/* Database */
 
 // Check connection
 db.once('open', function(){
@@ -17,21 +22,36 @@ db.on('error', function(err){
   console.log(err);
 });
 
+
+// initialize app
+const app = express();
+
+
+
+/* BodyParser */
+
+// body parser, for extracting data from POST bodies
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
 // import routes
 const index = require('./routes/index');
 const login = require('./routes/login');
 
-// initialize app
-const app = express()
 
 // view Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-// body parser, for extracting data from POST bodies
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+
+
+
+
+
+
+
 
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,9 +2,9 @@ const express 	= require('express');
 const router 	= express.Router();
 const mongoose 	= require('mongoose');
 
+
 // import models
 const Upload	= require('../models/Upload.model')
-
 
 // index route
 router.get('/', function(req, res, next){
@@ -15,13 +15,66 @@ router.get('/', function(req, res, next){
     })
 });
 
+
+
+/* /upload 
+
+    Q: how can this route handle single and article uploads to 
+    avoid redundancy...?
+
+*/
+
 router.get('/upload', function (req, res, next){
+
+
+   
+
     // render upload form
     res.render('upload.html')
 });
 
 router.post('/test', function(req, res, next){
+}
+
+
+router.post('/upload', function (req, res, next){
+
+
+    var url = req.body.url;
+    console.log('url ' + req.body.url);
+
+
+    var upload = new Upload({
+        title: 'Test Article',
+        description: 'Description for test article',
+        content: 'This is the content of the article',
+        tags: 'javascript,frontend',
+        type: 'article',
+        url: url,
+        uploadBy: 'Jonathan',
+        uploadDate: '21-06-2017 16:43:21',
+        originalAuthor: 'Joe Blogs',
+        originalCreationDate: '18-06-2017 16:43:21',
+    });
+
+
+    // save new upload
+    upload.save(function (err, upload) {
+        if (err) return console.error(err);
+        console.log('db :: saved ');
+        // return something if you want
+        // return guy
+    })
+
+
+    // render upload form
+    res.render('upload.html');
+});
+
+router.post('/', function(req, res, next){
 	//todo: persist url, add timestamp, and uploaded_by 
+
+
 
     // example creation code
     // var upload = new Upload({
